acf8e69c7b188045ae485f50b5f431fc76c35bce
joe <171643026+joe-238@users.noreply.github.com>
2025-10-14 18:06:50 -0400

    pluh

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import './main.css'
-import App from './App.vue'
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import { createApp } from "vue";
+import "./main.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+createApp(App).mount("#app");
